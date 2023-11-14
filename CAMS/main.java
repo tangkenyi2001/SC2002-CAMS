1402eaf32350c956a7045ff1c7ebdda4ceceb67a
tangkenyi2001 <73588464+tangkenyi2001@users.noreply.github.com>
2023-11-14 18:22:27 +0800

    update

--- CAMS/main.java
+++ CAMS/main.java
@@ -3,7 +3,17 @@ public class main {
         CampDatabase camper=new CampDatabase();
         camper.addCamps();
         Camp Camp0=camper.getCamps().get(0);
+
         System.out.println(Camp0.getCampInfo().getCampName());
+
+        System.out.println(Camp0.getCampInfo().getCampName()); 
+        CampDatabase a = new CampDatabase();
+        Student aStudent=new Student("nil", "nil", "nil","nil");
+        Staff aStaff = new Staff("thor" , "thor", "thor", "thor");
+        StaffMenu.printMenu(aStaff, CampDatabase.getCamps());
+
+
 
     }
 }
+
