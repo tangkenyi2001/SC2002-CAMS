d3e945d6511d20cf7e5ec3edeffb541b0e15fdc0
etienne <128112149+etienneboerner@users.noreply.github.com>
2023-11-26 19:41:16 +0800

    Update Serializer.java

--- CAMS/Serializer/Serializer.java
+++ CAMS/Serializer/Serializer.java
@@ -2,8 +2,16 @@ package Serializer;
 
 import java.io.*;
 import java.util.*;
-
+/**
+ * Represents a Serializer.
+ * @author Etienne Borner
+ * @version 1.0
+ * @since 2023-11-26
+ */
 public class Serializer {
+    /**
+     * Creates a serializer.
+     */
     public Serializer() {
         ;
     }
