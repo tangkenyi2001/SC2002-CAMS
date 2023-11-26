315f86c7d0b68943a3af42f2b5f45ab2df872e06
etienne <128112149+etienneboerner@users.noreply.github.com>
2023-11-26 18:39:25 +0800

    Update EnquiriesManager.java
    Added JavaDoc for EnquiriesManager

--- CAMS/Manager/EnquiriesManager.java
+++ CAMS/Manager/EnquiriesManager.java
@@ -8,38 +8,64 @@ import Entity.Camp;
 import Entity.Enquiry;
 import Entity.Student;
 import Serializer.*; 
-
-public class EnquiriesManager implements Serializable {
+/**
+ * Represents an EnquiryManager.
+ * @author Etienne Borner
+ * @version 1.0
+ * @since 2023-11-26
+ */
+public class EnquiriesManager {
+    /**
+     * ArrayList consisting of list of enquiries.
+     */
     private static ArrayList<Enquiry> enquiries;
     static EnquiriesSerializer s = new EnquiriesSerializer();
 
 
 public EnquiriesManager() {
-
+    /**
+     * Creates a new EnquiriesManager.
+     */
     enquiries = EnquiriesSerializer.getEnquiries("EnquiriesDatabase.ser"); // not sure if it should be CampSerializer here or s (TBC) // 
     if (enquiries == null) {
         enquiries = new ArrayList<Enquiry>();
         }
 }
+     /**
+     * Add enquiry to list of enquiries.
+     * Saves the new enquiry to the EnquiriesDatabase serialized file
+     * @param Enquiriy to be added.
+     */
 
 public static void addEnquiry(Enquiry newEnquiry) {
     enquiries.add(newEnquiry);
     EnquiriesSerializer.saveEnquiries(enquiries, "EnquiriesDatabase.ser");
    // System.out.println("Enquiries Successfully Saved.");
 }
+     /**
+     * Gets list of enquiries.
+     * 
+     * @return ArrayList of enquiries.
+     */
 public static ArrayList<Enquiry> getEnquiry() {
 		//System.out.println("Retrieving Enquiries.");
 		return enquiries;
 	}
-public static void clearEnquiries() {
-    // Clear the student list
-    enquiries.clear();
 
-    // Save the empty list to the serialized file
+     /**
+     * Clears the list of enquiries.
+     * Saves the empty list back to the EnquiriesDatabase serialized file
+     */
+
+public static void clearEnquiries() {
+    enquiries.clear();
     EnquiriesSerializer.saveEnquiries(enquiries, "EnquiriesDatabase.ser");
-    //System.out.println("Enquiries Database Cleared.");
 }
 
+     /**
+     * Updates the list of enquiries.
+     * @param Enquiry to be updated is saved to the ArrayList of enquiries.
+     */
 
 public static void update(Enquiry enquiry){
     int i=0;
